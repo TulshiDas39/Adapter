efb8ac1f7babd97532204efd53b2f96b4cfede13
Tulshi Das <37152713+TulshiDas39@users.noreply.github.com>
2018-11-05 18:57:17 +0600

    done

---
+++ src/adapter/Adapter.java
@@ -0,0 +1,13 @@
+package adapter;
+
+public class Adapter implements MediaPlayer{
+	public Adapter(AdvanceMediaPlayer ad){
+		
+	}
+
+	@Override
+	public void play() {
+		// TODO Auto-generated method stub
+		
+	}
+}
